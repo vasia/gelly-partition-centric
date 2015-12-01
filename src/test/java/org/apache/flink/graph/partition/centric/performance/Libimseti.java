4c01f853441b9ff14c375ec0100c3924cbd39a28
Nikola Koevski <koevskin@gmail.com>
2015-12-01 01:26:20 +0100

    Add Libimseti - dating website dataset and testing class

--- src/test/java/org/apache/flink/graph/partition/centric/performance/Libimseti.java
+++ src/test/java/org/apache/flink/graph/partition/centric/performance/Libimseti.java
@@ -4,23 +4,24 @@ import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.api.java.ExecutionEnvironment;
 import org.apache.flink.graph.Graph;
 import org.apache.flink.graph.Vertex;
-import org.apache.flink.graph.partition.centric.utils.GraphCCRunner;
 import org.apache.flink.graph.partition.centric.utils.GraphSSSPRunner;
 import org.apache.flink.types.NullValue;
 
 /**
- * Testing the PCSingleSourceShortestPaths on the US Airport Network dataset
+ * Testing the PCSingleSourceShortestPaths on the Libimseti dating website dataset.
  */
-public class USAirportsNetwork {
+public class Libimseti {
     public static void main(String[] args) throws Exception {
 
         ExecutionEnvironment environment = ExecutionEnvironment.getExecutionEnvironment();
         environment.getConfig().disableSysoutLogging();
 
         Graph<Long, Double, Double> graph = Graph
-                .fromCsvReader("data/us_airport_network/us_airport_network.data", environment)
-                .fieldDelimiterEdges(" ")
+                .fromCsvReader("data/libimseti/out.libimseti.data", environment)
+                .fieldDelimiterEdges("\t")
                 .lineDelimiterEdges("\n")
+                .ignoreCommentsEdges("%")
+                .ignoreInvalidLinesEdges()
                 .edgeTypes(Long.class, Double.class)
                 .mapVertices(new MapFunction<Vertex<Long, NullValue>, Double>() {
                     @Override
@@ -29,6 +30,6 @@ public class USAirportsNetwork {
                     }
                 });
 
-        GraphSSSPRunner.detectComponent(environment, graph, 1l, "out/pc_us_airports", "out/vc_us_airports");
+        GraphSSSPRunner.detectComponent(environment, graph, 1l, "out/pc_libimseti", "out/vc_libimseti");
     }
 }
