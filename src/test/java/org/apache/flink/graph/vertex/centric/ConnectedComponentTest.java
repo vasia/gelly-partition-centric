446f273e14fb9d92f1537baf29634575e689d44c
Kien Truong <duckientruong@gmail.com>
2015-12-08 16:58:33 +0100

    Add test for ConnectedComponent
    Add unittest to make sure that the vertex centric version of
    ConnectedComponent still produces the correct result after our
    modification.

---
+++ src/test/java/org/apache/flink/graph/vertex/centric/ConnectedComponentTest.java
@@ -0,0 +1,62 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.flink.graph.vertex.centric;
+
+import org.apache.flink.api.common.functions.RichMapFunction;
+import org.apache.flink.api.java.ExecutionEnvironment;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.graph.Graph;
+import org.apache.flink.graph.Vertex;
+import org.apache.flink.graph.partition.centric.PCConnectedComponents;
+import org.apache.flink.graph.partition.centric.utils.GraphGenerator;
+import org.apache.flink.test.testdata.ConnectedComponentsData;
+import org.junit.Test;
+
+import java.util.List;
+
+/**
+ * Test for our modification of ConnectedComponent algorithm implemented with
+ * vertex centric iteration
+ */
+public class ConnectedComponentTest {
+
+    @Test
+    public void testRun() throws Exception {
+        int verticesCount = 5000;
+        int edgesCount = verticesCount * 2;
+
+        ExecutionEnvironment environment = ExecutionEnvironment.getExecutionEnvironment();
+        environment.getConfig().disableSysoutLogging();
+
+        Graph<Long, Long, Long> graph = GraphGenerator.generateGraph(verticesCount, edgesCount, environment);
+
+        ConnectedComponents<Long, Long> algo = new ConnectedComponents<>(verticesCount);
+
+        List<Tuple2<Long, Long>> result = algo.run(graph).map(
+                new RichMapFunction<Vertex<Long, Long>, Tuple2<Long, Long>>() {
+                    @Override
+                    public Tuple2<Long, Long> map(Vertex<Long, Long> value) throws Exception {
+                        return new Tuple2<>(value.getId(), value.getValue());
+                    }
+                }).collect();
+
+        ConnectedComponentsData.checkOddEvenResult(result);
+    }
+}
